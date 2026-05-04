5eab4848ffce5195ba5268efa0b98bd4b79fccda
Lukas Wirth <lukas@zed.dev>
2026-05-04 12:39:38 +0200

    language_tools: Open server logs instead of server trace on View Logs (#55621)
    Release Notes:
    
    - N/A or Added/Fixed/Improved ...
    
    (cherry picked from commit 1264915af30935634bb90c4132b429e0e6ac09cb)

--- crates/language_tools/src/lsp_log_view.rs
+++ crates/language_tools/src/lsp_log_view.rs
@@ -29,7 +29,7 @@ use workspace::{
 
 use crate::get_or_create_tool;
 
-pub fn open_server_trace(
+pub fn open(
     log_store: &Entity<LogStore>,
     workspace: WeakEntity<Workspace>,
     server: LanguageServerSelector,
@@ -68,7 +68,7 @@ pub fn open_server_trace(
                             }
                         };
                         if let Some(server_id) = server_id {
-                            log_view.show_rpc_trace_for_server(server_id, window, cx);
+                            log_view.show_logs_for_server(server_id, window, cx);
                         }
                     });
                 })
