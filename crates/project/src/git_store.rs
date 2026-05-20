bf7aa90edff829d271dd8a91da05c912abb45de0
Cole Miller <cole@zed.dev>
2026-05-20 17:41:33 -0400

    Add some debug logging to understand hangs in the git store's `compute_snapshot` (#57317)
    Self-Review Checklist:
    
    - [x] I've reviewed my own diff for quality, security, and reliability
    - [x] Unsafe blocks (if any) have justifying comments
    - [x] The content is consistent with the [UI/UX
    checklist](https://github.com/zed-industries/zed/blob/main/CONTRIBUTING.md#uiux-checklist)
    - [ ] Tests cover the new/changed behavior
    - [x] Performance impact has been considered and is acceptable
    
    Release Notes:
    
    - N/A
    
    (cherry picked from commit 251456cf20887b91e0ba2d507b47ace14d4c24ff)

--- crates/project/src/git_store.rs
+++ crates/project/src/git_store.rs
@@ -9102,6 +9102,8 @@ async fn compute_snapshot(
     backend: Arc<dyn GitRepository>,
     cx: &mut AsyncApp,
 ) -> Result<RepositorySnapshot> {
+    log::debug!("starting compute snapshot");
+
     let (id, work_directory_abs_path, prev_snapshot) = this.update(cx, |this, _| {
         this.paths_needing_status_update.clear();
         (
@@ -9133,6 +9135,7 @@ async fn compute_snapshot(
             }
         })
         .await?;
+    log::debug!("fetched branches, head commit, worktrees");
     let branch = branches.iter().find(|branch| branch.is_head).cloned();
     let branch_list: Arc<[Branch]> = branches.into();
 
@@ -9155,6 +9158,8 @@ async fn compute_snapshot(
             }
         })
         .await?;
+
+    log::debug!("fetched remotes");
 
     let snapshot = this.update(cx, |this, cx| {
         let head_changed =
@@ -9215,6 +9220,7 @@ async fn compute_snapshot(
             }
         })
         .await?;
+    log::debug!("fetched statuses, diff stats, stash entries");
 
     let diff_stat_map: HashMap<&RepoPath, DiffStat> =
         diff_stats.entries.iter().map(|(p, s)| (p, *s)).collect();
