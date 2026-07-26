715bbfd2b69605eb259a58963c14cdd5dc81818e
BegoniaHe <tianyihebegonia1005@gmail.com>
2026-07-26 18:43:39 +0200

    sync: avoid truncating completion labels and docs
    Upstream: a3ac036eb6b73e0a50af4a44c96a43f1abf1b989
    Signed-off-by: BegoniaHe <tianyihebegonia1005@gmail.com>

--- crates/editor/src/code_context_menus.rs
+++ crates/editor/src/code_context_menus.rs
@@ -991,7 +991,7 @@ impl CompletionsMenu {
                                     .child(
                                         h_flex()
                                             .min_w_0()
-                                            .w_full()
+                                            .flex_grow()
                                             .when(left_aligned_suffix, |this| this.justify_start())
                                             .when(right_aligned_suffix, |this| {
                                                 this.justify_between()
@@ -1006,7 +1006,15 @@ impl CompletionsMenu {
                                                 this.child(div().truncate().child(suffix))
                                             }),
                                     )
-                                    .end_slot::<Label>(documentation_label),
+                                    .when_some(documentation_label, |this, doc| {
+                                        this.child(
+                                            div()
+                                                .flex_shrink()
+                                                .ml_auto()
+                                                .min_w_0()
+                                                .child(doc.truncate()),
+                                        )
+                                    }),
                             )
                     })
                     .collect()
