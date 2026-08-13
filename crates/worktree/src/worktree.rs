131b9d6d9910b7c0435139a65f82588a937f07d0
Kirill Bulatov <kirill@zed.dev>
2026-08-13 16:49:32 +0000

    Unify non-Unicode file detection code (#62581)
    Closes https://github.com/zed-industries/zed/issues/62464
    Closes https://github.com/zed-industries/zed/issues/62212
    
    As a bonus, fixes the project search not working in BOM'd UTF-16 files.
    
    Release Notes:
    
    - Fixed project search not working in some non-Unicode files
    
    (cherry picked from commit 4efba7161f848ab87be080f91bf28b06a7927c2c)
    Signed-off-by: BegoniaHe <tianyihebegonia1005@gmail.com>

--- crates/worktree/src/worktree.rs
+++ crates/worktree/src/worktree.rs
@@ -6865,7 +6865,7 @@ impl fs::Watcher for NullWatcher {
     }
 }
 
-async fn decode_file_text(
+pub async fn decode_file_text(
     fs: &dyn Fs,
     abs_path: &Path,
 ) -> Result<(String, &'static Encoding, bool)> {
@@ -6915,7 +6915,7 @@ async fn decode_file_text(
     decode_byte_full(content, bom_encoding, byte_content)
 }
 
-fn decode_byte_header(prefix: &[u8]) -> (Option<&'static Encoding>, ByteContent) {
+pub fn decode_byte_header(prefix: &[u8]) -> (Option<&'static Encoding>, ByteContent) {
     if let Some((encoding, _bom_len)) = Encoding::for_bom(prefix) {
         return (Some(encoding), ByteContent::Unknown);
     }
