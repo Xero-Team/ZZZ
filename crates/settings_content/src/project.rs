12d0fae5b806c558690e40bc02d6c9ae311c5d85
Marshall Bowers <git@maxdeviant.com>
2026-08-21 21:31:55 +0000

    git_hosting_providers: Add Tangled support (#63051)
    This PR adds support for [Tangled](https://tangled.org/) as a Git
    hosting provider.
    
    Release Notes:
    
    - Added support for permalinks to [Tangled](https://tangled.org/)
    repositories.
    
    (cherry picked from commit fd82517a115d97a07835b52f0512b22b38e38ccf)
    Signed-off-by: BegoniaHe <tianyihebegonia1005@gmail.com>

--- crates/settings_content/src/project.rs
+++ crates/settings_content/src/project.rs
@@ -808,7 +808,7 @@ pub enum DiagnosticSeverityContent {
 pub struct GitHostingProviderConfig {
     /// The type of the provider.
     ///
-    /// Must be one of `github`, `gitlab`, `bitbucket`, `gitea`, `forgejo`, or `source_hut`.
+    /// Must be one of `github`, `gitlab`, `bitbucket`, `gitea`, `forgejo`, `sourcehut`, or `tangled`.
     pub provider: GitHostingProviderKind,
 
     /// The base URL for the provider (e.g., "https://code.corp.big.com").
@@ -827,6 +827,7 @@ pub enum GitHostingProviderKind {
     Gitea,
     Forgejo,
     SourceHut,
+    Tangled,
 }
 
 #[cfg(test)]
